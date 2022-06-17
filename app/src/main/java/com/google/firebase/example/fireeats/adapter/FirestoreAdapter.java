82800d51a35b9561e7634705c5efae5c064ec603
johan52824 <58148047+johan52824@users.noreply.github.com>
2022-06-16 21:03:10 -0500

    practica.
    subeindo

---
+++ app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.java
@@ -0,0 +1,124 @@
+package com.google.firebase.example.fireeats.adapter;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+
+public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<VH>
+        implements EventListener<QuerySnapshot> { // Add this "implements"
+    // Add this method
+    @Override
+    public void onEvent(QuerySnapshot documentSnapshots,
+                        FirebaseFirestoreException e) {
+
+
+
+        // Dispatch the event
+        for (DocumentChange change : documentSnapshots.getDocumentChanges()) {
+            // Snapshot of the changed document
+            DocumentSnapshot snapshot = change.getDocument();
+
+            switch (change.getType()) {
+                case ADDED:
+                    onDocumentAdded(change); // Add this line
+                    break;
+                case MODIFIED:
+                    onDocumentModified(change); // Add this line
+                    break;
+                case REMOVED:
+                    onDocumentRemoved(change); // Add this line
+                    break;
+            }
+        }
+
+        onDataChanged();
+    }
+
+    protected void onDocumentAdded(DocumentChange change) {
+        mSnapshots.add(change.getNewIndex(), change.getDocument());
+        notifyItemInserted(change.getNewIndex());
+    }
+
+    protected void onDocumentModified(DocumentChange change) {
+        if (change.getOldIndex() == change.getNewIndex()) {
+            // Item changed but remained in same position
+            mSnapshots.set(change.getOldIndex(), change.getDocument());
+            notifyItemChanged(change.getOldIndex());
+        } else {
+            // Item changed and changed position
+            mSnapshots.remove(change.getOldIndex());
+            mSnapshots.add(change.getNewIndex(), change.getDocument());
+            notifyItemMoved(change.getOldIndex(), change.getNewIndex());
+        }
+    }
+
+    protected void onDocumentRemoved(DocumentChange change) {
+        mSnapshots.remove(change.getOldIndex());
+        notifyItemRemoved(change.getOldIndex());
+    }
+
+
+    private static final String TAG = "Firestore Adapter";
+
+    private Query mQuery;
+    private ListenerRegistration mRegistration;
+
+    private ArrayList<DocumentSnapshot> mSnapshots = new ArrayList<>();
+
+    public FirestoreAdapter(Query query) {
+        mQuery = query;
+    }
+
+    public void startListening() {
+        if (mQuery != null && mRegistration == null) {
+            mRegistration = mQuery.addSnapshotListener(this);
+        }
+    }
+
+    public void stopListening() {
+        if (mRegistration != null) {
+            mRegistration.remove();
+            mRegistration = null;
+        }
+
+        mSnapshots.clear();
+        notifyDataSetChanged();
+    }
+
+    public void setQuery(Query query) {
+        // Stop listening
+        stopListening();
+
+        // Clear existing data
+        mSnapshots.clear();
+        notifyDataSetChanged();
+
+        // Listen to new query
+        mQuery = query;
+        startListening();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSnapshots.size();
+    }
+
+    protected DocumentSnapshot getSnapshot(int index) {
+        return mSnapshots.get(index);
+    }
+
+    protected void onError(FirebaseFirestoreException e) {};
+
+    protected void onDataChanged() {}
+}
